9e563f35419fee977d98bf16d75dcae57058085d
Tanya <hanenko.tanya@gmail.com>
2023-11-10 20:28:46 +0300

    main branch

--- sup/mes/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mes'
--- sup/tic/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TicConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tic'
